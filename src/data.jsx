113f7b78431f7f35f029e9de02ce2fc307d1800c
joneshshrestha <contact@joneshshrestha.com>
2023-09-01 22:15:58 +0545

    update

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,7 @@
 import course1 from './images/course-1.jpeg'
 import course2 from './images/course-2.jpeg'
 import course3 from './images/course-3.jpeg'
+import course4 from './images/course-4.jpeg'
 
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
@@ -35,36 +36,49 @@ export const services = [
     text: 'Get in shape and improve your boxing performance with tailored workouts.',
   },
 ]
-
+// 18k 3days a week
+// 21k 4 days
+// 25k 5 days
+// 30k 6 days a week
 export const courses = [
   {
     id: '1',
     image: course1,
-    date: '4 WEEK PROGRAM',
+    date: '3 DAYS PER WEEK PROGRAM',
     title: 'Beginner Training',
     info: 'Learn fundamentals, stance, punches, defense. Expert coaching, dynamic workouts. Build strength, confidence, and skills. Perfect for beginners seeking fitness and self-defense.',
     buy: 'Enroll now!',
     // duration: '6 days',
-    cost: 'from $100',
+    cost: 'Rs 18000/month',
   },
   {
     id: '2',
     image: course2,
-    date: '8 WEEK PROGRAM',
+    date: '4 DAYS PER WEEK PROGRAM',
     title: 'Intermediate Training',
     info: 'Elevate your boxing prowess this course. Hone technique, amplify power, and refine defense through advanced drills and sparring, guided by Mac Mitts. Take your skills to the next level.',
     buy: 'Enroll now!',
     // duration: '11 days',
-    cost: 'from $500',
+    cost: 'Rs 21000/month',
   },
   {
     id: '3',
     image: course3,
-    date: '12 WEEK PROGRAM',
+    date: '5 DAYS PER WEEK PROGRAM',
     title: 'Advanced Training',
-    info: 'The pinnacle of boxing mastery. Fine-tune precision, strategy, and agility in intense sparring and specialized exercises, under the expert tutelage of Mac Mitts. Unleash your full potential.',
+    info: 'For experienced boxers who want to fine-tune precision, strategy, and agility in intense sparring and specialized exercises, under the expert tutelage of Mac Mitts. Unleash your full potential.',
     buy: 'Enroll now!',
     // duration: '8 days',
-    cost: 'from $1000',
+    cost: 'Rs 25000/month',
+  },
+  {
+    id: '4',
+    image: course4,
+    date: '6 DAYS PER WEEK PROGRAM',
+    title: 'Expert Training',
+    info: 'For those seeking the highest echelon of boxing excellence, this course is the ultimate training experience. This program is designed for elite fighters, aspiring professionals, and those with a deep passion for mastering the sweet science.',
+    buy: 'Enroll now!',
+    // duration: '8 days',
+    cost: 'Rs 30000/month',
   },
 ]
