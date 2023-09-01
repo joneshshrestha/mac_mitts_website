b7dc112a9abade00e80559ecd2b5989831327d7a
joneshshrestha <contact@joneshshrestha.com>
2023-09-01 21:58:34 +0545

    hambuger menu complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,43 @@
 import logo from '../images/logo.png'
 import { pageLinks, socialLinks } from '../data'
+import { useState } from 'react'
 
 function navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <nav className='navbar'>
       <div className='nav-center'>
         <div className='nav-header'>
-          <img src={logo} className='nav-logo' alt='backroads' />
-          <button type='button' className='nav-toggle' id='nav-toggle'>
-            <i className='fas fa-bars'></i>
-          </button>
+          <img src={logo} className='nav-logo' alt='mac-mitts' />
+          <div className='navbar-ham'>
+            <button
+              type='button'
+              className='toggle'
+              onClick={() => setNavbarOpen((prev) => !prev)}
+            >
+              {navbarOpen ? (
+                <i className='fa-solid fa-xmark'></i>
+              ) : (
+                <i className='fas fa-bars'></i>
+              )}
+            </button>
+          </div>
+          <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
+            {pageLinks.map((links) => {
+              return (
+                <li key={links.id}>
+                  <a
+                    href={links.href}
+                    className='nav-link'
+                    onClick={() => setNavbarOpen((prev) => !prev)}
+                  >
+                    {' '}
+                    {links.text}{' '}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
         </div>
         {/* <!-- left this comment on purpose --> */}
 
@@ -47,4 +75,23 @@ function navbar() {
     </nav>
   )
 }
+
+function handleClick() {
+  // $('#navLinks').toggleClass('active')
+  return (
+    <ul className='ham-links' id='ham-links'>
+      {pageLinks.map((links) => {
+        return (
+          <li key={links.id}>
+            <a href={links.href} className='ham-link'>
+              {' '}
+              {links.text}{' '}
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
 export default navbar
